124bb960d4a310fa329203632949cbe45257da60
mnguyen <mnguyen@evenica.com>
2024-02-28 18:52:47 -0500

    .

--- src/app/api/minify/route.ts
+++ src/app/api/minify/route.ts
@@ -1,3 +1,7 @@
-export async function POST() {
+import { NextRequest } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const cookie = request.cookies
+  console.log(cookie)
   return Response.json({ ok: 'o' })
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { matchUrl } from '@bo-carey/urlglob'
 import { NextRequest, NextResponse } from 'next/server'
 
-const ALLOWED_ORIGINS = ['https://**mobius.cloud**', 'https://blank.page', 'null']
-
+const CORS_PREFIX = 'Access-Control-Allow-'
+const DEV_ORIGINS = ['https://blank.page', 'null']
+const BASE_ORIGIN = 'https://**mobius.cloud**'
+const IGNORED_PATHS = ['passwordreset', 'login']
 const CORS_BASE_OPTION = {
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+  [getCorsKey('Headers')]: 'Content-Type, Authorization',
+  [getCorsKey('Methods')]: 'POST,OPTIONS',
 }
 
 export const config = {
@@ -13,22 +15,41 @@ export const config = {
 }
 
 export async function middleware(request: NextRequest) {
-  const origin = request.headers.get('origin') ?? 'null'
-  const matches = ALLOWED_ORIGINS.map((url) => matchUrl(origin, url))
-  const isAllowedOrigin = matches.some((value) => value)
-
-  if (request.method === 'OPTIONS') {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-      ...CORS_BASE_OPTION,
-    }
-    return NextResponse.json({}, { headers: preflightHeaders })
-  }
-
+  const isAllowedOrigin = checkOrigin(request)
   const response = NextResponse.next()
-
-  if (isAllowedOrigin) response.headers.set('Access-Control-Allow-Origin', origin)
+  if (isAllowedOrigin) response.headers.set(getCorsKey('Origin'), origin)
   Object.entries(CORS_BASE_OPTION).forEach(([key, value]) => response.headers.set(key, value))
-
   return response
+}
+
+function checkOrigin(request: NextRequest) {
+  const origin = request.headers.get('origin') as string
+  const ignoredOrigins = IGNORED_PATHS.map((path) => `${BASE_ORIGIN}${path}**`)
+  const ignoredMatches = ignoredOrigins.map((value) => matchUrl(origin, value))
+  const devMatches = DEV_ORIGINS.map((value) => matchUrl(origin, value))
+
+  const isDevMode = process.env.NODE_ENV === 'development'
+  const isDevOrigin = devMatches.some((value) => value)
+  const isIgnoredOrigin = ignoredMatches.some((value) => !value)
+  const isBaseOrigin = matchUrl(origin, BASE_ORIGIN)
+  const isAllowedOrigin = (isBaseOrigin && !isIgnoredOrigin) || (isDevMode && isDevOrigin)
+
+  const isPreflight = request.method === 'OPTIONS'
+
+  if (isPreflight)
+    return NextResponse.json(
+      {},
+      {
+        headers: {
+          ...(isAllowedOrigin && { [getCorsKey('Origin')]: origin }),
+          ...CORS_BASE_OPTION,
+        },
+      },
+    )
+
+  return isAllowedOrigin
+}
+
+function getCorsKey(postfix: string) {
+  return CORS_PREFIX + postfix
 }
