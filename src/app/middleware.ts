0df458a51f6283f13d676d8ab01d50bc79e6f4c8
mnguyen <mnguyen@evenica.com>
2024-02-28 14:27:28 -0500

    .

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,1 +1,18 @@
-export async function middleware() {}
+import { NextResponse } from 'next/server'
+
+export async function middleware() {
+  const res = NextResponse.next()
+
+  const corsHeaders = [
+    ['Access-Control-Allow-Credentials', 'true'],
+    ['Access-Control-Allow-Origin', '*'],
+    ['Access-Control-Allow-Methods', 'GET,POST'],
+    [
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    ],
+  ]
+  corsHeaders.forEach(([key, value]) => res.headers.append(key, value))
+
+  return res
+}
